1980268087b0975db7b053e5300afcaa4828755f
JaiderYesid <150965073+JaiderYesid@users.noreply.github.com>
2024-11-05 12:45:22 -0500

    Add files via upload

---
+++ SQLITE - INVENTECH/flaskr/vistas/vistas.py
@@ -0,0 +1,18 @@
+from flask import request
+from flask_restful import Resource
+from ..modelos import db, Proveedor, ProveedorSchema
+
+proveedor_schema = ProveedorSchema()
+
+class VistaProveedor(Resource):
+    def get(self):
+        return [proveedor_schema.dump(Proveedor) for Proveedor in Proveedor.query.all()]
+
+    def post(self):
+        nuevo_proveedor = Proveedor(nombre=request.json['nombre'],\
+                                    email=request.json['email'],\
+                                    direccion=request.json['direccion'],\
+                                    telefono=request.json['telefono'])
+        db.session.add(nuevo_proveedor)
+        db.session.commit()
+        return proveedor_schema.dump(nuevo_proveedor), 201
